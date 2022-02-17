c77f9a6bc126716eb2b9a9b0b8822da809dc1f79
Moopz-Dev <pongzthor@gmail.com>
2022-02-17 09:52:51 +0700

    fixed endpoint bugs for story CRUDs

--- migrations/20220208090344-create-story.js
+++ migrations/20220208090344-create-story.js
@@ -1,41 +1,40 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Stories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      user_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: {
-            tableName: 'users',
-          },
-        },
-      },
-      message: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      media: {
-        type: Sequelize.STRING(1234),
-        allowNull: false,
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Stories');
-  },
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("Stories", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			user_id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: {
+						tableName: "users",
+					},
+				},
+			},
+			message: {
+				type: Sequelize.STRING,
+			},
+			media: {
+				type: Sequelize.STRING(1234),
+				allowNull: false,
+			},
+			created_at: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updated_at: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("Stories");
+	},
 };
