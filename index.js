24a174f284726cd874991de28b1d9d8450d3d8ab
thanapol <thanapol_khunkoel@hotmail.com>
2022-02-19 18:57:20 +0700

    bnfix

--- index.js
+++ index.js
@@ -12,27 +12,27 @@ app.use(morgan("tiny"));
 app.use(cors());
 
 // routes
-fs.readdirSync("./routes").map(r => app.use("", require("./routes/" + r)));
+fs.readdirSync("./routes").map((r) => app.use("", require("./routes/" + r)));
 
 //error handling
 app.use((req, res) => {
-	res.status(404).json({ message: "resource not found on this server" });
+  res.status(404).json({ message: "resource not found on this server" });
 });
 
 app.use((err, req, res, next) => {
-	console.log(err);
-	let code = 500;
-	if (err.name === "JsonWebTokenError" || err.name === "TokenExpiredError") {
-		code = 401;
-	}
-	if (process.env.NODE_ENV === "development") {
-		res.status(code).json({ error: err.message });
-	} else {
-		res.status(code).json({ message: " Your mom is sad." });
-	}
+  console.log(err);
+  let code = 500;
+  if (err.name === "JsonWebTokenError" || err.name === "TokenExpiredError") {
+    code = 401;
+  }
+  if (process.env.NODE_ENV === "development") {
+    res.status(code).json({ error: err.message });
+  } else {
+    res.status(code).json({ message: " Your mom is sad." });
+  }
 });
 
 //server hosting
 app.listen(process.env.PORT || 8765, () =>
-	console.log("server listening on port " + process.env.PORT || 8765)
+  console.log("server listening on port " + process.env.PORT || 8765)
 );
