46f9c093881932398b1b5be6b538e220d436f14e
KittitachNil <blackhawkdown_208@hotmail.com>
2022-02-20 19:46:43 +0700

    edited postcontroller and add s at attribute

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const postController = require('../controllers/postController');
 const {
-  authenticateUser,
-  authenticateAdminOrUser,
+    authenticateUser,
+    authenticateAdminOrUser,
 } = require('../middlewares/authenticate');
 const router = express.Router();
 
 router.get('/posts/all', authenticateUser, postController.getAllPosts);
 router.get(
-  '/posts/user/:userId',
-  authenticateUser,
-  postController.getUserPosts
+    '/posts/user/:userId',
+    authenticateUser,
+    postController.getUserPosts
 );
 router.post('/posts/', authenticateUser, postController.createPost);
 router.patch('/posts/:id', authenticateUser, postController.updatePost);
-router.patch('/posts/:id', authenticateUser, postController.updatePostMedia);
 router.delete('/posts/:id', authenticateUser, postController.deletePost);
 
 module.exports = router;
