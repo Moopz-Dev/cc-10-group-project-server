3d73cbc01f2080e3f585b1ecdf21697da04ed7d8
Moopz-Dev <pongzthor@gmail.com>
2022-02-22 11:11:55 +0700

    fix path parameter bug

--- routes/likeRoute.js
+++ routes/likeRoute.js
@@ -6,21 +6,21 @@ const {
 } = require("../middlewares/authenticate");
 const router = express.Router();
 
-router.post("like/story/:id", authenticateUser, likeController.likeStory);
+router.post("/like/story/:id", authenticateUser, likeController.likeStory);
 router.post(
-	"like/story-comment/:id",
+	"/like/story-comment/:id",
 	authenticateUser,
 	likeController.likeStoryComment
 );
-router.post("like/reel/:id", authenticateUser, likeController.likeReel);
+router.post("/like/reel/:id", authenticateUser, likeController.likeReel);
 router.post(
-	"like/reel-comment/:id",
+	"/like/reel-comment/:id",
 	authenticateUser,
 	likeController.likeReelComment
 );
-router.post("like/post/:id", authenticateUser, likeController.likePost);
+router.post("/like/post/:id", authenticateUser, likeController.likePost);
 router.post(
-	"like/post-comment/:id",
+	"/like/post-comment/:id",
 	authenticateUser,
 	likeController.likePostComment
 );
