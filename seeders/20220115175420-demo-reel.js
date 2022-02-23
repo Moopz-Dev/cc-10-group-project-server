d06f7e8f956874a8213c06d00f2770d6ec2e8c1b
Moopz-Dev <pongzthor@gmail.com>
2022-02-23 12:28:08 +0700

    add new seed pictures

--- seeders/20220115175420-demo-reel.js
+++ seeders/20220115175420-demo-reel.js
@@ -7,7 +7,7 @@ module.exports = {
 			[
 				{
 					message: "Test message for Reel 1",
-					media: "https://picsum.photos/200/300",
+					media: "https://picsum.photos/id/1/200/300",
 					song: "https://instrumentalfx.co/wp-content/upload/11/Unison-Aperture-NCS-Release.mp3",
 					user_id: 2,
 					created_at: new Date(),
@@ -15,7 +15,7 @@ module.exports = {
 				},
 				{
 					message: "Test message for Reel 2",
-					media: "https://picsum.photos/200/300",
+					media: "https://picsum.photos/id/2/200/300",
 					song: "https://instrumentalfx.co/wp-content/upload/11/Unison-Aperture-NCS-Release.mp3",
 					user_id: 2,
 					created_at: new Date(),
@@ -23,7 +23,7 @@ module.exports = {
 				},
 				{
 					message: "Test message for Reel 3",
-					media: "https://picsum.photos/200/300",
+					media: "https://picsum.photos/id/3/200/300",
 					song: "https://instrumentalfx.co/wp-content/upload/11/Unison-Aperture-NCS-Release.mp3",
 					user_id: 3,
 					created_at: new Date(),
@@ -31,7 +31,7 @@ module.exports = {
 				},
 				{
 					message: "Test message for Reel 4",
-					media: "https://picsum.photos/200/300",
+					media: "https://picsum.photos/id/4/200/300",
 					song: "https://instrumentalfx.co/wp-content/upload/11/Unison-Aperture-NCS-Release.mp3",
 					user_id: 3,
 					created_at: new Date(),
@@ -39,7 +39,7 @@ module.exports = {
 				},
 				{
 					message: "Test message for Reel 5",
-					media: "https://picsum.photos/200/300",
+					media: "https://picsum.photos/id/5/200/300",
 					song: "https://instrumentalfx.co/wp-content/upload/11/Unison-Aperture-NCS-Release.mp3",
 					user_id: 4,
 					created_at: new Date(),
@@ -47,7 +47,7 @@ module.exports = {
 				},
 				{
 					message: "Test message for Reel 6",
-					media: "https://picsum.photos/200/300",
+					media: "https://picsum.photos/id/6/200/300",
 					song: "https://instrumentalfx.co/wp-content/upload/11/Unison-Aperture-NCS-Release.mp3",
 					user_id: 4,
 					created_at: new Date(),
@@ -55,7 +55,7 @@ module.exports = {
 				},
 				{
 					message: "Test message for Reel 7",
-					media: "https://picsum.photos/200/300",
+					media: "https://picsum.photos/id/7/200/300",
 					song: "https://instrumentalfx.co/wp-content/upload/11/Unison-Aperture-NCS-Release.mp3",
 					user_id: 6,
 					created_at: new Date(),
